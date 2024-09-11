30c2ee0a235ca3c0f475abfe47540f7af9d7d1a2
zaru <zarutofu@gmail.com>
2024-09-12 08:29:46 +0900

    sample compoentn and add example theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import {
+  Button,
+  Label,
+  ListBox,
+  ListBoxItem,
+  Popover,
+  Select,
+  SelectValue,
+} from "react-aria-components";
 
 function App() {
   return (
     <>
       <div>aa</div>
+      <Select>
+        <Label>Favorite Animal</Label>
+        <Button>
+          <SelectValue />
+          <span aria-hidden="true">▼</span>
+        </Button>
+        <Popover>
+          <ListBox>
+            <ListBoxItem>Cat</ListBoxItem>
+            <ListBoxItem>Dog</ListBoxItem>
+            <ListBoxItem>Kangaroo</ListBoxItem>
+          </ListBox>
+        </Popover>
+      </Select>
     </>
   );
 }
