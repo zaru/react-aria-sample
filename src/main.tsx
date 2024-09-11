8ee6790fd555ac6f6098fbf97a23a9186658498c
zaru <zarutofu@gmail.com>
2024-09-12 08:23:58 +0900

    add biome

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+const root = document.getElementById("root");
+if (!root) throw new Error("No root element");
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
